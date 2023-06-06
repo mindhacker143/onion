b31b894f35f76dd5f0c658690b46dd73e8296c00
mindhacker143 <mohammadaltaf9418@gmail.com>
2023-06-06 02:25:13 -0700

    onion

---
+++ src/brocli.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Brocli() {
+  return (
+    <div className='h-full w-full bg-slate-100'></div>
+  )
+}
+
+export default Brocli
---
+++ src/coliflower.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Coliflower() {
+  return (
+    <div className='h-72 w-full  flex'>
+        <div className='justify-between p-[90px] flex h-full w-full items-center border-2'>
+       
+       
+        <div className='h-[200px] w-[200px] bg-slate-200'></div>
+       
+       
+       
+        <div className='h-[200px] w-[900px] space-y-4'>
+              <div className='h-10 w-full bg-slate-200'></div>
+              <div className='h-4 w-full bg-slate-200'></div>
+              <div className='h-4 w-full bg-slate-200'></div>
+              <div className='h-4 w-full bg-slate-200'></div>
+              <div className='h-4 w-full bg-slate-200'></div>
+              <div className='h-4 w-full bg-slate-200'></div>
+
+
+        </div>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Coliflower
